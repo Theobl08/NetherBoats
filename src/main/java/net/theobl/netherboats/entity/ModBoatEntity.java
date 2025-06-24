b68da06b5824f722989e114ce92f0311d6c3ecc7
Theobl08 <98655331+Theobl08@users.noreply.github.com>
2025-06-24 21:41:47 +0200

    Remove unused imports

--- src/main/java/net/theobl/netherboats/entity/ModBoatEntity.java
+++ src/main/java/net/theobl/netherboats/entity/ModBoatEntity.java
@@ -1,30 +1,15 @@
 package net.theobl.netherboats.entity;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.util.ByIdMap;
-import net.minecraft.util.StringRepresentable;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractBoat;
-import net.minecraft.world.entity.vehicle.Boat;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.fluids.FluidType;
 import net.theobl.netherboats.Config;
-import net.theobl.netherboats.NetherBoats;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
-import java.util.function.IntFunction;
 import java.util.function.Supplier;
 
 public class ModBoatEntity extends AbstractBoat {
--- src/main/java/net/theobl/netherboats/entity/ModChestBoatEntity.java
+++ src/main/java/net/theobl/netherboats/entity/ModChestBoatEntity.java
@@ -1,25 +1,14 @@
 package net.theobl.netherboats.entity;
 
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.vehicle.AbstractChestBoat;
-import net.minecraft.world.entity.vehicle.Boat;
-import net.minecraft.world.entity.vehicle.ChestBoat;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.FireBlock;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.fluids.FluidType;
 import net.theobl.netherboats.Config;
-import net.theobl.netherboats.NetherBoats;
-import org.jetbrains.annotations.NotNull;
 import java.util.function.Supplier;
 
 public class ModChestBoatEntity extends AbstractChestBoat {
